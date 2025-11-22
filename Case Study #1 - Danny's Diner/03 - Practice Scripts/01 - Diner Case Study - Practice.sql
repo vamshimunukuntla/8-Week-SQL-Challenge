1a69fee20c95a815a3e3da61d4a06ffeb0b7d44f
vamshimunukuntla <vamshimunukuntla9@gmail.com>
2025-11-23 02:58:36 +0530

    Add files via upload

---
+++ Case Study #1 - Danny's Diner/03 - Practice Scripts/01 - Diner Case Study - Practice.sql
@@ -0,0 +1,204 @@
+-- Databricks notebook source
+-- CATALOG - sql_challenge
+-- SCHEMA - diner
+
+-- COMMAND ----------
+
+-- TABLES - sales, menu, members
+
+-- COMMAND ----------
+
+-- MAGIC
+-- MAGIC %md
+-- MAGIC ## Join Tables
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### SALES - MENU
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### SALES - MENU - MEMBERS
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Load Tables
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### sales_details
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### diner_data
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Default Catalog and Database
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## CASE STUDY QUESTIONS
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 1 . What is the total amount each customer spent at the restaurant?
+-- MAGIC
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 2. How many days has each customer visited the restaurant?
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 3. What was the first item from the menu purchased by each customer?
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### Version 1
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### Version 2
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 5. Which item was the most popular for each customer?
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### Version 1
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC #### Version 2
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 6. Which item was purchased first by the customer after they became a member?
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 7. Which item was purchased just before the customer became a member?
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 8. What is the total items and amount spent for each member before they became a member?
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 9. If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 10.In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January? 
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 11. Join all the things
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### 12. Rank all the things
+
+-- COMMAND ----------
+
